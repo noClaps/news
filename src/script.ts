9327e4bca0f47c2e83712fa2d9ea7712c7138fc7
noClaps <git@zerolimits.dev>
2025-01-25 12:04:44 +0000

    feat: Remove worker and get chronological (slow) feed

--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,15 @@
-const mainElement = document.querySelector("main")!;
+const BLOCKLIST = [
+  "news.ycombinator.com",
+  "twitter.com",
+  "www.wsj.com",
+  "www.bloomberg.com",
+];
+
+interface Story {
+  type: "story";
+  url: string;
+  title: string;
+}
 
 const storyIds: number[] = await fetch(
   "https://hacker-news.firebaseio.com/v0/newstories.json",
@@ -8,32 +19,26 @@ const storyIds: number[] = await fetch(
     throw new Error(err);
   });
 
-const MAX_CONCURRENT_WORKERS = navigator.hardwareConcurrency;
-let activeWorkers = 0;
-let currentIndex = 0;
+const mainElement = document.querySelector("main")!;
+for (const storyId of storyIds) {
+  const story: Story = await fetch(
+    `https://hacker-news.firebaseio.com/v0/item/${storyId}.json`,
+  )
+    .then((r) => r.json())
+    .catch((err) => {
+      throw new Error(err);
+    });
 
-function processNextStory() {
   if (
-    currentIndex >= storyIds.length ||
-    activeWorkers >= MAX_CONCURRENT_WORKERS
+    story.type !== "story" ||
+    !story.url ||
+    BLOCKLIST.includes(new URL(story.url).hostname)
   ) {
-    return;
+    continue;
   }
 
-  const storyId = storyIds[currentIndex++];
-  activeWorkers++;
-
-  const worker = new Worker("/worker.js", { type: "module" });
-  worker.postMessage(storyId);
-
-  worker.addEventListener("message", ({ data }) => {
-    if (data) mainElement.insertAdjacentHTML("beforeend", data);
-    worker.terminate();
-    activeWorkers--;
-    processNextStory();
-  });
-}
-
-for (let i = 0; i < MAX_CONCURRENT_WORKERS; i++) {
-  processNextStory();
+  mainElement.insertAdjacentHTML(
+    "beforeend",
+    `<a href="${story.url}" target="_blank">${story.title} <span class="dim">(${new URL(story.url).hostname})</span></a><hr>`,
+  );
 }
